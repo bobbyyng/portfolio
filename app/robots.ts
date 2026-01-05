b8bd56f1bf8b90f1726e573c56f2ad21a5117d44
bobbyyng <httbrs116@gmail.com>
2026-01-05 21:04:43 +0800

    chore(robots): replace robots.txt with programmatic robots route to manage web crawler access

---
+++ app/robots.ts
@@ -0,0 +1,33 @@
+// app/robots.ts
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'Googlebot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Google-Extended',
+        disallow: '/',
+      },
+      {
+        userAgent: 'GPTBot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'ClaudeBot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'meta-externalagent',
+        disallow: '/',
+      },
+    ],
+  };
+}
